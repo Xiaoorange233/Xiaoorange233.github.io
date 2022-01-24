a9c022636cd53250a4b2616ea2560249754c953c
Xiaoorange233 <51257469+Xiaoorange233@users.noreply.github.com>
2022-01-25 00:39:15 +0800

    修复预加载无法转圈的BUG

--- js/preload.js
+++ js/preload.js
@@ -1,4 +1,3 @@
-document.write('<link rel="stylesheet" type="text/css" href="./css/perload.css">');
 document.write('<div id="preloader" style="justify-content: center;align-items: center;height: 100vh;display: flex;width: 100%;"><img src="./img/preloader.png" height="64px" alt=""></div>');
 document.onreadystatechange = function(){
     if(document.readyState == 'complete'){
