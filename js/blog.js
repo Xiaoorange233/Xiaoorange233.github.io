12a2fca992b8fdefa15be40f4eb2b1771316fe19
Xiaoorange233 <51257469+Xiaoorange233@users.noreply.github.com>
2022-01-24 23:42:44 +0800

    新增版本2.0 支持Issues博客

---
+++ js/blog.js
@@ -0,0 +1,116 @@
+function getlist(){
+	$.get({
+		url : "https://api.github.com/repos/xiaoorange233/xiaoorange233.github.io/issues",
+		dataType : "json",
+		success : function(data) {
+			if(!data.hasOwnProperty('message')){
+				for(i=0;i in data;i++){
+					var label = "";
+					var img ="";
+					for(n=0;n in data[i].labels;n++){
+						var label= label+'<span class="badge rounded-pill bg-primary">'+data[i].labels[n].name+'</span> '
+					}
+					$('#list').append('<a href="./blog_body.html?page='+data[i].number+'" target="_blank"><li class="list-group-item"><h4>'+data[i].title+'</h4>'+label+'</li>');
+				}
+			}else{
+				alert('系统异常，请重新刷新后再试');
+			}
+		},
+		error : function(data) {
+			if(data.status == 403){
+				alert('操作过于频繁，请稍后再试');
+			}else if(data.status == 0){
+				alert('无法连接至服务器，请检查网络设置');
+			}else if(data.status == 404){
+				alert('系统异常，请稍后再试');
+			}else if(data.status == 200){
+				alert('系统异常，请重新刷新后再试');
+			}else{
+				alert(data.msg);
+			}
+		}
+	});
+}
+function getbody(){
+	if(!getvalue('page')){
+		$("#blog_body").html('<h3>页面载入出错！</h3>');
+	}else{
+		var pagev=getvalue('page');
+	}
+	var body = "";
+	$.get({
+		url : "https://api.github.com/repos/xiaoorange233/xiaoorange233.github.io/issues/"+pagev,
+		dataType : "json",
+		success : function(data) {
+			if(!data.hasOwnProperty('message')){
+				$.post({
+					url : "https://api.github.com/markdown",
+					data: JSON.stringify({'text':data.body}),
+					dataType : "html",
+					success : function(data) {
+						$("#blog_body").html(data);
+					},
+					error : function(data) {
+						if(data.status == 403){
+							alert('操作过于频繁，请稍后再试');
+							$("#blog_body").html('<h3>操作过于频繁，请稍后再试</h3>');
+							return;
+						}else if(data.status == 0){
+							alert('无法连接至服务器，请检查网络设置');
+							$("#blog_body").html('<h3>无法连接至服务器，请检查网络设置</h3>');
+							return;
+						}else if(data.status == 404){
+							alert('系统异常，请稍后再试');
+							$("#blog_body").html('<h3>系统异常，请稍后再试</h3>');
+							return;
+						}else if(data.status == 200){
+							alert('系统异常，请重新刷新后再试');
+							$("#blog_body").html('<h3>系统异常，请重新刷新后再试</h3>');
+							return;
+						}else{
+							alert(data.msg);
+							$("#blog_body").html('<p>'+data.msg+'</p>');
+							return;
+						}
+					}
+				});
+			}else{
+				$("#blog_body").html('<h3>系统异常，请重新刷新后再试</h3>');
+				return;
+			}
+		},
+		error : function(data) {
+			if(data.status == 403){
+				alert('操作过于频繁，请稍后再试');
+				$("#blog_body").html('<h3>操作过于频繁，请稍后再试</h3>');
+				return;
+			}else if(data.status == 0){
+				alert('无法连接至服务器，请检查网络设置');
+				$("#blog_body").html('<h3>无法连接至服务器，请检查网络设置</h3>');
+				return;
+			}else if(data.status == 404){
+				alert('系统异常，请稍后再试');
+				$("#blog_body").html('<h3>系统异常，请稍后再试</h3>');
+				return;
+			}else if(data.status == 200){
+				alert('系统异常，请重新刷新后再试');
+				$("#blog_body").html('<h3>系统异常，请重新刷新后再试</h3>');
+				return;
+			}else{
+				alert(data.msg);
+				$("#blog_body").html('<p>'+data.msg+'</p>');
+				return;
+			}
+		}
+	});
+}
+function getvalue(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
+}
---
+++ js/preload.js
@@ -0,0 +1,6 @@
+document.write('<div id="preloader" style="justify-content: center;align-items: center;height: 100vh;display: flex;width: 100%;"><img src="./img/preloader.png" height="64px" alt=""></div>');
+document.onreadystatechange = function(){
+    if(document.readyState == 'complete'){
+        document.getElementById("preloader").style.display="none";
+    }
+}
