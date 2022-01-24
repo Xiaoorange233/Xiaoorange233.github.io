069338b1a4b617655c62a4079d9c393f3b336b15
Xiaoorange233 <51257469+Xiaoorange233@users.noreply.github.com>
2022-01-24 23:44:23 +0800

    Add files via upload

--- js/preload.js
+++ js/preload.js
@@ -1,3 +1,4 @@
+document.write('<link rel="stylesheet" type="text/css" href="./css/perload.css">');
 document.write('<div id="preloader" style="justify-content: center;align-items: center;height: 100vh;display: flex;width: 100%;"><img src="./img/preloader.png" height="64px" alt=""></div>');
 document.onreadystatechange = function(){
     if(document.readyState == 'complete'){
